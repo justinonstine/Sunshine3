518a9a595bbc7e53095907b518ce841b7ddbcc6f
justinonstine <justin_onstine@hotmail.com>
2015-03-02 14:13:18 -0800

    first commit of sunshine
    this is the first commit, somewhere around the start of lesson 4A of
    the android developer udacity course.

---
+++ app/src/main/java/com/example/erin/sunshine3/app/DetailActivity.java
@@ -0,0 +1,96 @@
+package com.example.erin.sunshine3.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class DetailFragment extends Fragment {
+
+        String weatherData;
+        public DetailFragment() {
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
+
+            Intent intent = getActivity().getIntent();
+            weatherData = intent.getStringExtra(Intent.EXTRA_TEXT);
+            textView.setText(weatherData);
+            return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detail_fragment, menu);
+            MenuItem shareItem = menu.findItem(R.id.action_share);
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
+            }
+        }
+
+        private Intent createShareIntent() {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, weatherData + " (brought to you by loving fiance's app)");
+            return intent;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/erin/sunshine3/app/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.erin.sunshine3.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    public void onStart() {
+        Log.v("rotateMonitor", "OnStart!");
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        Log.v("rotateMonitor", "OnStop!");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v("rotateMonitor", "OnDestroy!");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        Log.v("rotateMonitor", "OnPause!");
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        Log.v("rotateMonitor", "OnResume!");
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v("rotateMonitor", "OnCreate!");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
